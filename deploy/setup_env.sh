c6de91050b3ab213d224f9fadacec087337c339b
PeterJochem <peterjochem2020@u.northwestern.edu>
2022-07-09 17:12:05 -0400

    Add a bash script to deploy the code to an EC2 instance

---
+++ deploy/setup_env.sh
@@ -0,0 +1,19 @@
+# Install dependencies for the backend
+
+#sudo apt update
+#sudo apt install -y npm
+#sudo npm -g install create-react-app
+
+#sudo apt install -y docker
+#sudo apt install -y docker-compose
+#sudo apt install -y postgresql postgresql-contrib
+#sudo systemctl start postgresql.service
+
+#sudo apt install -y software-properties-common
+#sudo add-apt-repository ppa:deadsnakes/ppa
+#sudo apt install -y python3.9
+#sudo apt install -y python3.9-venv
+
+python3.9 -m venv ../backend/server/venv
+
+
